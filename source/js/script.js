823b71eb96b522c5508e007e0888671fcb2484b4
Sergey <10rubleqow@gmail.com>
2020-03-07 00:45:22 +0300

    Внес правки валидаторов и линтера

--- source/js/script.js
+++ source/js/script.js
@@ -17,19 +17,11 @@
   var PIN_FOURTH_STEP = 1237;
 
   var toggleLabels = document.querySelectorAll('.page-header__label');
-  var toggleInputs = document.querySelectorAll('.page-header__input');
   var wrapper = document.querySelector('.wrapper');
   var footer = document.querySelector('.page-footer');
-  var dateWrapper = footer.querySelector('.toggle__date-wrapper');
   var main = document.querySelector('main');
-  var pages = main.querySelector('.page-main__slide');
-  var firstPage = main.querySelector('.first-page');
-  var secondPage = main.querySelector('.second-page');
   var thirdPage = main.querySelector('.third-page');
   var thirdPageWrapper = thirdPage.querySelector('.third-page__wrapper');
-  var thirdPageFirstSlide = thirdPage.querySelector('.first-slide');
-  var thirdPageSecondSlide = thirdPage.querySelector('.second-slide');
-  var thirdPageThirdSlide = thirdPage.querySelector('.third-slide');
   var slideDownWrapper = footer.querySelector('.page-footer__slide-down-wrapper');
   var toggleThirdPage = footer.querySelector('.page-footer__toggle');
   var toggleLine = footer.querySelector('.toggle__line');
@@ -70,51 +62,51 @@
     moveY: undefined,
     index: FIRST_PAGE,
     longTouch: undefined,
-    init: function() {
+    init: function () {
       this.bindUIEvents();
     },
-    bindUIEvents: function() {
-      this.el.holder.addEventListener("touchstart", function(event) {
+    bindUIEvents: function () {
+      this.el.holder.addEventListener('touchstart', function (event) {
         slider.start(event);
       });
-      this.el.holder.addEventListener("touchmove", function(event) {
+      this.el.holder.addEventListener('touchmove', function (event) {
         slider.move(event);
       });
-      this.el.holder.addEventListener("touchend", function(event) {
+      this.el.holder.addEventListener('touchend', function (event) {
         slider.end(event);
       });
     },
-    start: function(event) {
+    start: function (event) {
       this.longTouch = false;
-      setTimeout(function() {
+      setTimeout(function () {
         slider.longTouch = true;
       }, TIMEOUT);
-      this.touchStartY =  event.touches[0].pageY;
+      this.touchStartY = event.touches[0].pageY;
       if (this.el.holder.classList.contains('animate')) {
         this.el.holder.classList.remove('animate');
       }
     },
-    move: function(event) {
-      this.touchMoveY =  event.touches[0].pageY;
-      this.moveY = this.index*this.slideHeight + (this.touchStartY - this.touchMoveY);
+    move: function (event) {
+      this.touchMoveY = event.touches[0].pageY;
+      this.moveY = this.index * this.slideHeight + (this.touchStartY - this.touchMoveY);
       if (this.moveY < PAGE_MOVE_STEP) {
         this.el.holder.style.transform = 'translate3d(0,-' + this.moveY + 'px,0)';
       } else if (this.moveY > PAGE_MOVE_SECOND_STEP && this.moveY < PAGE_MOVE_THIRD_STEP) {
         this.el.holder.style.transform = 'translate3d(0,-' + this.moveY + 'px,0)';
       }
     },
-    end: function(event) {
-      var absMove = Math.abs(this.index*this.slideHeight - this.moveY);
-      if (absMove > this.slideHeight/DEFAULT_PAGE_FACTOR || this.longTouch === false) {
-        if (this.moveY > this.index*this.slideHeight && this.index < THIRD_PAGE) {
+    end: function () {
+      var absMove = Math.abs(this.index * this.slideHeight - this.moveY);
+      if (absMove > this.slideHeight / DEFAULT_PAGE_FACTOR || this.longTouch === false) {
+        if (this.moveY > this.index * this.slideHeight && this.index < THIRD_PAGE) {
           this.index++;
-        } else if (this.moveY < this.index*this.slideHeight && this.index > FIRST_PAGE) {
+        } else if (this.moveY < this.index * this.slideHeight && this.index > FIRST_PAGE) {
           this.index--;
         }
       }
       toggleSliders(this.index);
       this.el.holder.classList.add('animate');
-      this.el.holder.style.transform = 'translate3d(0,-' + this.index*this.slideHeight + 'px,0)';
+      this.el.holder.style.transform = 'translate3d(0,-' + this.index * this.slideHeight + 'px,0)';
     }
   };
 
@@ -135,37 +127,37 @@
     pinMoveX: undefined,
     index: THIRD_PAGE,
     longTouch: undefined,
-    init: function() {
+    init: function () {
       this.el.holder.classList.add('animate');
-      this.el.holder.style.transform = 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)';
-      this.el.line.style.width = this.index*HALF_LINE_WIDTH + 'px';
+      this.el.holder.style.transform = 'translate3d(-' + this.index * this.slideWidth + 'px,0,0)';
+      this.el.line.style.width = this.index * HALF_LINE_WIDTH + 'px';
       this.bindUIEvents();
     },
-    bindUIEvents: function() {
-      this.el.pin.addEventListener("touchstart", function(event) {
+    bindUIEvents: function () {
+      this.el.pin.addEventListener('touchstart', function (event) {
         thirdPageSlider.start(event);
       });
-      this.el.pin.addEventListener("touchmove", function(event) {
+      this.el.pin.addEventListener('touchmove', function (event) {
         thirdPageSlider.move(event);
       });
-      this.el.pin.addEventListener("touchend", function(event) {
+      this.el.pin.addEventListener('touchend', function (event) {
         thirdPageSlider.end(event);
       });
     },
-    start: function(event) {
+    start: function (event) {
       this.longTouch = false;
-      setTimeout(function() {
+      setTimeout(function () {
         thirdPageSlider.longTouch = true;
       }, TIMEOUT);
-      this.touchStartX =  event.touches[0].pageX;
+      this.touchStartX = event.touches[0].pageX;
       if (this.el.holder.classList.contains('animate')) {
         this.el.holder.classList.remove('animate');
       }
     },
-    move: function(event) {
-      this.touchMoveX =  event.touches[0].pageX;
-      this.moveX = this.index*this.slideWidth + -(this.touchStartX - this.touchMoveX);
-      this.pinMoveX = this.index*HALF_LINE_WIDTH + -(this.touchStartX - this.touchMoveX);
+    move: function (event) {
+      this.touchMoveX = event.touches[0].pageX;
+      this.moveX = this.index * this.slideWidth + -(this.touchStartX - this.touchMoveX);
+      this.pinMoveX = this.index * HALF_LINE_WIDTH + -(this.touchStartX - this.touchMoveX);
       this.el.holder.style.transform = 'translate3d(-' + this.moveX + 'px,0,0)';
 
       if (this.index === FIRST_PAGE) {
@@ -185,18 +177,18 @@
       }
 
     },
-    end: function(event) {
-      var absMove = Math.abs(this.index*this.slideWidth - this.moveX);
-      if (absMove > this.slideWidth/DEFAULT_SLIDER_FACTOR || this.longTouch === false) {
-        if (this.moveX > this.index*this.slideWidth && this.index < THIRD_PAGE) {
+    end: function () {
+      var absMove = Math.abs(this.index * this.slideWidth - this.moveX);
+      if (absMove > this.slideWidth / DEFAULT_SLIDER_FACTOR || this.longTouch === false) {
+        if (this.moveX > this.index * this.slideWidth && this.index < THIRD_PAGE) {
           this.index++;
-        } else if (this.moveX < this.index*this.slideWidth && this.index > FIRST_PAGE) {
+        } else if (this.moveX < this.index * this.slideWidth && this.index > FIRST_PAGE) {
           this.index--;
         }
       }
       this.el.holder.classList.add('animate');
-      this.el.holder.style.transform = 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)';
-      this.el.line.style.width = this.index*HALF_LINE_WIDTH + 'px';
+      this.el.holder.style.transform = 'translate3d(-' + this.index * this.slideWidth + 'px,0,0)';
+      this.el.line.style.width = this.index * HALF_LINE_WIDTH + 'px';
     }
   };
 
